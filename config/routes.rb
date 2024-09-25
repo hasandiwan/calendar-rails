b60e3fec337695814cae4dd08d3e4d0eafa81e00
Hasan Diwan <hasan@gooutside.com>
2024-09-25 04:44:06 +0000

    make schema more responsive to our needs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,28 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :authentications do
+    resources :events, only: :index
+  end
+  #root "home#index"
+  get  "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+  get  "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  resources :sessions, only: [:index, :show, :destroy]
+  resource  :password, only: [:edit, :update]
+  namespace :identity do
+    resource :email,              only: [:edit, :update]
+    resource :email_verification, only: [:show, :create]
+    resource :password_reset,     only: [:new, :edit, :create, :update]
+  end
+  get  "/auth/failure",            to: "sessions/omniauth#failure"
+  get  "/auth/:provider/callback", to: "sessions/omniauth#create"
+  post "/auth/:provider/callback", to: "sessions/omniauth#create"
+  namespace :sessions do
+    resource :sudo, only: [:new, :create]
+  end
+  #root "home#index"
   resources :events
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   post '/qr', to: 'events#qr'
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,14 +10,51 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_220_511_182_548) do
-  create_table 'events', force: :cascade do |t|
-    t.string 'title', null: false
-    t.datetime 'start', null: false
-    t.datetime 'end', null: false
-    t.string 'color', limit: 7
-    t.boolean 'all_day', default: false, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+ActiveRecord::Schema[7.0].define(version: 2024_09_25_034819) do
+  create_table "email_verification_tokens", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_email_verification_tokens_on_user_id"
   end
+
+  create_table "events", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title"
+    t.datetime "start"
+    t.datetime "end"
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "password_reset_tokens", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_password_reset_tokens_on_user_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "user_agent"
+    t.string "ip_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.boolean "verified", default: false, null: false
+    t.string "provider"
+    t.string "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "calendar_id"
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  add_foreign_key "email_verification_tokens", "users"
+  add_foreign_key "events", "users"
+  add_foreign_key "password_reset_tokens", "users"
+  add_foreign_key "sessions", "users"
 end
