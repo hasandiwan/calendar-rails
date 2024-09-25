79efa05b058f5e9cf04396cf52e4b615e2934b2d
Hasan Diwan <hasan@gooutside.com>
2024-09-25 04:40:36 +0000

    generate QR codes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -65,14 +65,17 @@ class EventsController < ApplicationController
     qr = RQRCode::QRCode.new(params[:text])
     png = qr.as_png
     IO::binwrite("#{Rails.root}/public/qr.png", png)
-    url = URI('https://api.imgbb.com/1/upload?key=904f9669f28a4b17ced781bb497d7d5e')
-    header = {'Content-Type': 'image/png'}
-    http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Post.new(uri.request_uri, header)
-    request.body = Base64.encode64(IO.binread("#{Rails.root}/public/qr.png"))
-    response = http.request(request)
-    imgbb_hash = JSON.parse(response.body)
-    ret = {:string => qr.to_s, :remote => imgbb_hash[:data][:image][:url], :svg => qr.as_svg( color: "000", shape_rendering: "crispEdges", module_size: 11, standalone: true, use_path: true), text: params[:text], :time =>  (Time.now.to_f * 1000).to_i-start}
+    # FIXME
+    #url = URI('https://api.imgbb.com/1/upload?key=904f9669f28a4b17ced781bb497d7d5e')
+    #header = {'Content-Type': 'image/png'}
+    #http = Net::HTTP.new(url.host, url.port)
+    #http.use_ssl = true
+    #request = Net::HTTP::Post.new(url.request_uri, header)
+    #request.body = Base64.encode64(IO.binread("#{Rails.root}/public/qr.png"))
+    #response = http.request(request)
+    #imgbb_hash = JSON.parse(response.body)
+    #byebug
+    ret = {:string => qr.to_s,:svg => qr.as_svg( color: "000", shape_rendering: "crispEdges", module_size: 11, standalone: true, use_path: true), text: params[:text], :time =>  (Time.now.to_f * 1000).to_i-start}
     render json: ret
     return
   end
@@ -89,10 +92,6 @@ class EventsController < ApplicationController
 
     def convert_date(date)
       I18n.l(date&.to_datetime || Time.zone.now)
-    end
-
-    def auth
-      Current.account != nil
     end
 
     def set_event
--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,4 +1,27 @@
 # frozen_string_literal: true
 
+#require 'icalendar'
+#require 'open-uri'
 module EventsHelper
+class Event::IcalendarEvent
+  include Rails.application.routes.url_helpers
+
+  def initialize(event:, user: nil)
+    @event = {}
+    @user = user 
+  end
+
+  def call
+     cal = Icalendar::Calendar.parse(URI.open(@user[:calendar_id]).read)
+
+     cal.events.each { |e|
+       if e.dtstart.after(Date.today) and e.dtend.before(Date.tomorrow) then # event is today
+         # TODO make all-day event detection smarter -- https://stackoverflow.com/questions/4330672/all-day-event-icalendar-gem
+         @user.events.append(Event.new(title:e.title , start:e.dtstart, end:e.dtend, all_day: false)).save!
+       end
+     }
+  end
 end
+
+end
+
